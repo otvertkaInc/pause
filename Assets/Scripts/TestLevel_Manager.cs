31605aff2abcf25ed6386c69552afc135dfbe6d1
TezRomacH <tez.romach@gmail.com>
2015-11-08 18:31:52 +0300

    Теперь есть фигура - круг. И цели у все фигур

---
+++ Assets/Scripts/TestLevel_Manager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestLevel_Manager : MonoBehaviour
+{
+    void OnGUI()
+    {
+        //List<ShapeBehaviour> shapes = new List<ShapeBehaviour>(FindObjectsOfType<ShapeBehaviour>());
+        
+        //if(isAllTrue(shapes))
+            GUI.Label(new Rect(10, 10, 100, 20), "You win!");
+    }
+
+    /// Проверяет все ли фигуры на своих финальных позициях
+    bool isAllTrue(List<ShapeBehaviour> shapes)
+    {
+        bool res = true;
+        int i = 0;
+        while(res & (i < shapes.Count))
+        {
+            res &= shapes[i].isFinalPosition;
+        }
+
+        return res;
+    }
+}
---
+++ Assets/Scripts/TriggerCheck.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class TriggerCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /// Срабатывает при входе объекта в триггер
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        ShapeBehaviour shape = GetComponentInParent<ShapeBehaviour>();
+        if (col.name.StartsWith(shape.shape_name + "ChangeDirection"))
+        {
+            shape.direction = col.GetComponent<DirChanger>().new_direction - transform.position;
+        }
+
+        if (col.name.StartsWith(shape.shape_name + "RightPosition"))
+        {
+            shape.isFinalPosition = true;
+        }
+    }
+
+    /// Срабатывает при входе объекта в триггер
+    void OnTriggerExit2D(Collider2D col)
+    {
+        ShapeBehaviour shape = GetComponentInParent<ShapeBehaviour>();
+        if (col.name.StartsWith(shape.shape_name + "RightPosition"))
+        {
+            shape.isFinalPosition = false;
+        }
+    }
+}
