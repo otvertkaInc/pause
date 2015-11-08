68fea5ca4e1f2a84dd4c9339d31936880fd9ffb1
TezRomacH <tez.romach@gmail.com>
2015-11-08 13:26:06 +0300

    Starter
    Двигающийся треугольник, останавливающийся по клику

---
+++ Assets/Scripts/DirChanger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirChanger : MonoBehaviour
+{
+
+    public Vector3 new_direction;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/ShapeBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+public class ShapeBehaviour : MonoBehaviour
+{
+
+    public float speed = 0.5f;
+
+    public Vector3 direction;
+    public string shape_name = "Shape";
+
+    private bool isMoving = true;
+	
+    /// Создает начальные условия
+	void Start ()
+    {
+        direction = GameObject.Find(shape_name + "ChangeDirection1").GetComponent<DirChanger>().transform.position
+                    - transform.position;
+    }
+	
+    /// Обновляется несколько раз за кадр
+	void FixedUpdate () {
+        if(isMoving)
+            transform.Translate(direction * Time.deltaTime * speed);
+	}
+
+    /// Срабатывает при входе объекта в триггер
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == shape_name + "ChangeDirection")
+        {
+            direction = col.GetComponent<DirChanger>().new_direction - transform.position;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isMoving = !isMoving;
+    }
+}
