9025d829af8d79c4ba958741ade310e4d293d3ee
mamikon13 <m.nickogosyan@mail.ru>
2015-11-29 00:11:47 +0300

    Добита ф-ия moveToFinalPos; когда комментите, ставьте пробел перед комментом в коде!

--- Assets/Scripts/levelController.cs
+++ Assets/Scripts/levelController.cs
@@ -3,16 +3,6 @@ using System.Collections.Generic;
 
 public class levelController : MonoBehaviour
 {
-    /// <summary>
-    /// Массив ChangeDirection'ов для треугольника
-    /// </summary>
-    private List<DirChanger> triangle_changes;
-
-    /// <summary>
-    /// Массив ChangeDirection'ов для окружности
-    /// </summary>
-    private List<DirChanger> circle_changes;
-
     private string shape;
 
     /// <summary>
@@ -31,7 +21,7 @@ public class levelController : MonoBehaviour
     bool allTrue;
 
     /// <summary>
-    /// Функция сравнения цисел типа float (точность = 0.01f)
+    /// Функция сравнения чисел типа float (точность = 0.01f)
     /// </summary>
     bool compareFloat(float x, float y, float eps = 0.01f)
     {
@@ -45,21 +35,39 @@ public class levelController : MonoBehaviour
     {
         List<ShapeMovement> shapes = new List<ShapeMovement>(FindObjectsOfType<ShapeMovement>());
         int countOfRightSh = 0;
+        int countOfRightRotSh = 0;
+        float finalRotSpeed = 9f;
         foreach (ShapeMovement sh in shapes)
         {
             Vector3 rightPos = GameObject.Find(sh.shape_name + "RightPosition").transform.position;
             Vector3 direction = rightPos - sh.transform.position; // Здесь как раз таки не нужно использовать .normalized
             sh.GetComponent<Rigidbody2D>().velocity = direction * 2.5f;
             if (sh.GetComponent<ShapeRotation>() != null)
-                //sh.transform.eulerAngles = new Vector3(0.0f, sh.transform.rotation.y, sh.transform.rotation.z);
-                sh.transform.eulerAngles = new Vector3(Mathf.LerpAngle(sh.transform.rotation.x, 0.0f, 5 * Time.fixedDeltaTime), sh.transform.rotation.y, sh.transform.rotation.z);
+            {
+                // Условия для замедления скорости вращения
+                if (Mathf.Abs(sh.GetComponent<Rigidbody2D>().rotation) < 30f)
+                    finalRotSpeed = 2f;
+                if (Mathf.Abs(sh.GetComponent<Rigidbody2D>().rotation) < 15f)
+                    finalRotSpeed = 1f;
+                if (Mathf.Abs(sh.GetComponent<Rigidbody2D>().rotation) < 5f)
+                    finalRotSpeed = 0.2f;
+                
+                if (!compareFloat(sh.GetComponent<Rigidbody2D>().rotation, 0f))
+                {
+                    sh.transform.RotateAround(sh.transform.position, Vector3.forward, finalRotSpeed);
+                }                                                                        // без else
+                if (compareFloat(sh.GetComponent<Rigidbody2D>().rotation, 0f))
+                {
+                    countOfRightRotSh++;
+                }
+            }
             if (compareFloat(sh.transform.position.x, rightPos.x) && compareFloat(sh.transform.position.y, rightPos.y))
             {
                 sh.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                 countOfRightSh++;
             }
         }
-        if (countOfRightSh == shapes.Count)
+        if (countOfRightSh == shapes.Count && countOfRightRotSh == 1)
         {
             inCycle = false; // Ограничение на вход в цикл
             flagForGUI_level_manager = true; // для проверки в скрипте GUI_level_manager.cs
@@ -68,22 +76,16 @@ public class levelController : MonoBehaviour
 
     void Start()
     {
-        // для triangle
-        shape = GameObject.Find("red_triangle").GetComponent<ShapeBehaviour>().shape_name;
-        triangle_changes = new List<DirChanger>(GameObject.Find(shape + "_changes").GetComponentsInChildren<DirChanger>()); // создается массив точек отталкивания фигуры triangle
-        int count_triangle = triangle_changes.Count;
-        for (int i = 0; i < count_triangle; ++i)          // инициализация направления движения
+        List<ShapeMovement> shapes = new List<ShapeMovement>(FindObjectsOfType<ShapeMovement>()); // создается список всех фигур
+        foreach (ShapeMovement sh in shapes)
         {
-            triangle_changes[i].new_direction = triangle_changes[(i + 1) % count_triangle].transform.position;
-        }
-
-        // для circle
-        shape = GameObject.Find("blue_circle").GetComponent<ShapeBehaviour>().shape_name;
-        circle_changes = new List<DirChanger>(GameObject.Find(shape + "_changes").GetComponentsInChildren<DirChanger>()); // создается массив точек отталкивания фигуры circle
-        int count_circle = circle_changes.Count;
-        for (int i = 0; i < count_circle; ++i)          // инициализация направления движения
-        {
-            circle_changes[i].new_direction = circle_changes[(i + 1) % count_circle].transform.position;
+            shape = sh.GetComponent<ShapeBehaviour>().shape_name;
+            List<DirChanger> changes = new List<DirChanger>(GameObject.Find(shape + "_changes").GetComponentsInChildren<DirChanger>()); // создается список точек отталкивания очередной фигуры
+            int count = changes.Count;
+            for (int i = 0; i < count; ++i)          // инициализация направления движения
+            {
+                changes[i].new_direction = changes[(i + 1) % count].transform.position;
+            }
         }
 
         inCycle = true;
